40a0aac821f84612aa3c8d6f461b82db6cf5a0b4
z80 <z80@ophy.xyz>
2023-10-05 09:39:18 -0400

    Begin writing functions for syntax feature detection
    move types to own file

--- src/main.py
+++ src/main.py
@@ -4,36 +4,41 @@ from lsprotocol.types import (
     TEXT_DOCUMENT_COMPLETION,
     CompletionParams,
 )
-
+import re
 from pygls.lsp.types.language_features import CompletionOptions, CompletionList, CompletionItem
+from types import BASE_TYPES
+
 
 server = LanguageServer('vyper', 'v0.0.1')
 
+# detect if current line is a variable declaration
+def is_var_declaration(line):
 
-@server.feature(TEXT_DOCUMENT_COMPLETION, CompletionOptions(trigger_characters=[":"]))
+    # regex for variable declaration
+    # should match lines starting with any identifier followed by a colon
+    # like "foo: "
+    reg = r"^\s*[a-zA-Z_][a-zA-Z0-9_]*\s*:"
+    # print to stderr
+    return bool(re.match(reg, line.strip()))
+
+@server.feature(TEXT_DOCUMENT_COMPLETION, CompletionOptions(trigger_characters=[":", " "]))
 def completions(ls, params: CompletionParams):
     items = []
     document = server.workspace.get_document(params.text_document.uri)
     og_line = document.lines[params.position.line]
     current_line = document.lines[params.position.line].strip()
-    print(current_line, file=sys.stderr)
-    if current_line.endswith(":"):
-        if og_line[-1] != " ":
-            items = [
-                CompletionItem(label="uint256", insert_text=" uint256"),
-                CompletionItem(label="address", insert_text=" address"),
-                CompletionItem(label="bool", insert_text=" bool"),
-            ]
-        else:
-            items = [
-                    CompletionItem(label="uint256"),
-                    CompletionItem(label="address"),
-                    CompletionItem(label="bool"),
-                ]
-    print(items, file=sys.stderr)
-    l = CompletionList(is_incomplete=False, items=[])
-    l.add_items(items)
-    return l
+
+    if is_var_declaration(og_line):
+        for typ in BASE_TYPES:
+            if og_line.endswith(" "):
+                items.append(CompletionItem(label=typ))
+            else:
+                items.append(CompletionItem(label=typ, insert_text=f" {typ}"))
+
+        l = CompletionList(is_incomplete=False, items=[])
+        l.add_items(items)
+        return l
+    return
 
 def main():
     server.start_io()
