8fc37d142dd929880a04e3c893da525c1e6c1604
z80 <z80@ophy.xyz>
2023-10-11 12:17:13 -0400

    move logic out of main.py

--- src/main.py
+++ src/main.py
@@ -1,62 +1,91 @@
-from pygls.server import LanguageServer
 import sys
+from typing import Optional
 from lsprotocol.types import (
     TEXT_DOCUMENT_COMPLETION,
+    TEXT_DOCUMENT_DID_CHANGE,
+    TEXT_DOCUMENT_DID_OPEN,
+    TEXT_DOCUMENT_DECLARATION,
+    TEXT_DOCUMENT_DEFINITION
 )
-import re
-from pygls.lsp.types.language_features import CompletionOptions, CompletionList, CompletionItem, CompletionParams
-from .types import BASE_TYPES
-from vyper.ast.grammar import parse_vyper_source
+from pygls.lsp.types import (
+    CompletionOptions,
+    CompletionParams,
+    DidChangeTextDocumentParams,
+    DidOpenTextDocumentParams,
+)
+from pygls.lsp.types.language_features import (
+    DeclarationOptions,
+    DeclarationParams,
+    DefinitionParams,
+    Location,
+    Position,
+    Range,
+)
+from pygls.server import LanguageServer
 
-DECORATORS = ["public", "private", "constant", "payable", "nonpayable", "view", "pure", "external"]
+from src.completions import Completer
+from src.utils import get_word_at_cursor
 
-server = LanguageServer('vyper', 'v0.0.1')
+from .ast import AST
+from .diagnostics import get_diagnostics
 
-# detect if current line is a variable declaration
-def is_var_declaration(line):
+server = LanguageServer("vyper", "v0.0.1")
+completer = Completer()
+ast = AST()
 
-    # regex for variable declaration
-    # should match lines starting with any identifier followed by a colon
-    # like "foo: "
-    reg = r"^\s*[a-zA-Z_][a-zA-Z0-9_]*\s*:"
-    # print to stderr
-    return bool(re.match(reg, line.strip()))
+def validate_doc(ls, params):
+    text_doc = ls.workspace.get_document(params.text_document.uri)
+    diagnostics = get_diagnostics(text_doc.source)
+    ls.publish_diagnostics(params.text_document.uri, diagnostics)
+    ast.update_ast(text_doc)
 
-def is_attribute_access(line):
-    # regex for attribute access
-    # should match lines ending with a dot
-    # like "foo."
-    reg = r"\s*\.\s*$"
-    return bool(re.match(reg, line.strip()))
 
-@server.feature(TEXT_DOCUMENT_COMPLETION, CompletionOptions(trigger_characters=[":", ".", "@"]))
+@server.feature(TEXT_DOCUMENT_DID_OPEN)
+async def did_open(ls: LanguageServer, params: DidOpenTextDocumentParams):
+    validate_doc(ls, params)
+
+
+@server.feature(TEXT_DOCUMENT_DID_CHANGE)
+async def did_change(ls: LanguageServer, params: DidChangeTextDocumentParams):
+    validate_doc(ls, params)
+
+
+@server.feature(
+    TEXT_DOCUMENT_COMPLETION, CompletionOptions(trigger_characters=[":", ".", "@", " "])
+)
 def completions(ls, params: CompletionParams):
-    items = []
-    document = server.workspace.get_document(params.text_document.uri)
+    return completer.get_completions(ls, params)
+
+
+@server.feature(TEXT_DOCUMENT_DECLARATION)
+def go_to_declaration(ls: LanguageServer, params: DeclarationParams) -> Optional[Location]:
+    document = ls.workspace.get_document(params.text_document.uri)
     og_line = document.lines[params.position.line]
-    current_line = document.lines[params.position.line].strip()
+    word = get_word_at_cursor(og_line, params.position.character)
+    node = ast.find_declaration_node(word)
+    if node:
+        range = Range(
+            start=Position(line=node.lineno - 1, character=node.col_offset),
+            end=Position(line=node.end_lineno - 1, character=node.end_col_offset),
+        )
+        declaration: Location = Location(uri=params.text_document.uri, range=range)
+        return declaration
 
-    if params.context:
-        if params.context.trigger_character == ".":
-            print(f"trigger character is {params.context.trigger_character}", file=sys.stderr)
-        elif params.context.trigger_character == "@":
-            print(f"trigger character is {params.context.trigger_character}", file=sys.stderr)
-            for dec in DECORATORS:
-                items.append(CompletionItem(label=dec))
-            l = CompletionList(is_incomplete=False, items=[])
-            l.add_items(items)
-            return l
-        elif params.context.trigger_character == ":":
-            for typ in BASE_TYPES:
-                if og_line.endswith(" "):
-                    items.append(CompletionItem(label=typ))
-                else:
-                    items.append(CompletionItem(label=typ, insert_text=f" {typ}"))
+@server.feature(TEXT_DOCUMENT_DEFINITION)
+def go_to_definition(ls: LanguageServer, params: DefinitionParams) -> Optional[Location]:
+    document = ls.workspace.get_document(params.text_document.uri)
+    og_line = document.lines[params.position.line]
+    word = get_word_at_cursor(og_line, params.position.character)
+    node = ast.find_declaration_node(word)
+    if node:
+        range = Range(
+            start=Position(line=node.lineno - 1, character=node.col_offset),
+            end=Position(line=node.end_lineno - 1, character=node.end_col_offset),
+        )
+        declaration: Location = Location(uri=params.text_document.uri, range=range)
+        return declaration
 
-            l = CompletionList(is_incomplete=False, items=[])
-            l.add_items(items)
-            return l
-    return CompletionList(is_incomplete=False, items=[])
+
 
 def main():
     server.start_io()
