8f6f0a57238143758bb7a0725f48deaefb06e072
z80 <z80@ophy.xyz>
2023-10-26 01:48:08 -0400

    extend find reference tests

--- tests/test_navigation.py
+++ tests/test_navigation.py
@@ -4,13 +4,57 @@ from pygls.workspace import Document
 from vyper_lsp.ast import AST
 from vyper_lsp.navigation import ASTNavigator
 
+import pytest
 
-def test_find_references():
+
+@pytest.fixture
+def doc():
     doc = Document(uri="examples/Foo.vy")
-    pos = Position(line=2, character=7)
     ast = AST()
     ast.build_ast(doc.source)
-    navigator = ASTNavigator(ast)
-    print(doc.source)
+    return doc
+
+
+@pytest.fixture
+def navigator():
+    return ASTNavigator()
+
+
+def test_find_references_event_name(doc, navigator):
+    pos = Position(line=2, character=7)
+    references = navigator.find_references(doc, pos)
+    assert len(references) == 3
+
+
+def test_find_references_struct_name(doc, navigator):
+    pos = Position(line=6, character=7)
+    references = navigator.find_references(doc, pos)
+    assert len(references) == 4
+
+
+def test_find_references_enum_name(doc, navigator):
+    pos = Position(line=9, character=7)
+    references = navigator.find_references(doc, pos)
+    assert len(references) == 5
+
+
+def test_find_references_enum_variants(doc, navigator):
+    pos = Position(line=10, character=7)
+    references = navigator.find_references(doc, pos)
+    assert len(references) == 1
+
+    pos = Position(line=11, character=7)
+    references = navigator.find_references(doc, pos)
+    assert len(references) == 1
+
+
+def test_find_references_function_name(doc, navigator):
+    pos = Position(line=40, character=5)
+    references = navigator.find_references(doc, pos)
+    assert len(references) == 1
+
+
+def test_find_references_storage_var(doc, navigator):
+    pos = Position(line=13, character=0)
     references = navigator.find_references(doc, pos)
     assert len(references) == 3
